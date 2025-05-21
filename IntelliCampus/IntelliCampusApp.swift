a3c0d2ed1fdcacc1c567edfc481a5076cd45989f
Gourishankar Bansode <gourishankar2710@gmail.com>
2025-05-20 17:47:52 -0700

    Initial Commit

---
+++ IntelliCampus/IntelliCampusApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntelliCampusApp.swift
+//  IntelliCampus
+//
+//  Created by Gourishankar Bansode on 5/20/25.
+//
+
+import SwiftUI
+
+@main
+struct IntelliCampusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IntelliCampusTests/IntelliCampusTests.swift
@@ -0,0 +1,17 @@
+//
+//  IntelliCampusTests.swift
+//  IntelliCampusTests
+//
+//  Created by Gourishankar Bansode on 5/20/25.
+//
+
+import Testing
+@testable import IntelliCampus
+
+struct IntelliCampusTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
